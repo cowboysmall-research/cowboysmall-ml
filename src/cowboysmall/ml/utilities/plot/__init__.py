197a185d929135becf0bdd08d517c34019a257dc
Jerry Kiely <jerry@cowboysmall.com>
2023-08-08 23:38:47 +0100

    added scatterplot utilities + some cleanup

--- src/cowboysmall/ml/utilities/plot/__init__.py
+++ src/cowboysmall/ml/utilities/plot/__init__.py
@@ -3,3 +3,4 @@ from cowboysmall.ml.utilities.plot.confusion_matrix import *
 from cowboysmall.ml.utilities.plot.histogram        import *
 from cowboysmall.ml.utilities.plot.learning_curve   import *
 from cowboysmall.ml.utilities.plot.roc_curve        import *
+from cowboysmall.ml.utilities.plot.scatterplot      import *
