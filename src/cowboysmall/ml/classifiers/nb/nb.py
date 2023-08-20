bfb812bdd84bdfc7964c573f74d89224b8012889
Jerry Kiely <jerry@cowboysmall.com>
2023-08-20 12:55:51 +0100

    further simplification of the original impl

--- src/cowboysmall/ml/classifiers/nb/nb.py
+++ src/cowboysmall/ml/classifiers/nb/nb.py
@@ -14,18 +14,18 @@ class NaiveBayes:
                 self.stats[label][feature] = (np.mean(X[y == label, feature]), np.var(X[y == label, feature]))
 
     def predict(self, X):
-        predictions = []
+        predictions = np.empty(X.shape[0])
 
-        for row in X:
+        for i in range(X.shape[0]):
             posteriors = {}
             for label in self.priors:
                 posterior = self.priors[label]
-                for feature in range(row.shape[0]):
-                    posterior *= self.gaussian(row[feature], self.stats[label][feature])
+                for feature in range(X[i].shape[0]):
+                    posterior *= self.gaussian(X[i][feature], self.stats[label][feature])
                 posteriors[label] = posterior
-            predictions.append(max(posteriors, key = posteriors.get))
+            predictions[i] = max(posteriors, key = posteriors.get)
 
-        return np.array(predictions)
+        return predictions
 
     def gaussian(self, x, stats):
         return np.exp(-np.square(x - stats[0]) / (2 * stats[1])) / np.sqrt(2 * (np.pi) * stats[1])
