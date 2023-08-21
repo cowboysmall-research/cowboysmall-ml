acbe7cfc595749830ad98ebdb8dfed379da40af4
Jerry Kiely <jerry@cowboysmall.com>
2023-08-21 22:35:37 +0100

    cleaned up naive bayes impl + fixed an issue around max label selection

--- src/cowboysmall/ml/classifiers/nb/nb.py
+++ src/cowboysmall/ml/classifiers/nb/nb.py
@@ -1,32 +1,41 @@
 import numpy as np
 
-from collections import Counter
-
 
 class NaiveBayes:
 
     def fit(self, X, y):
-        self.priors = {key: float(value) / len(y) for key, value in dict(Counter(y)).items()}
-        self.stats  = {label: dict() for label in np.unique(y)}
+        labels, counts = np.unique(y, return_counts = True)
 
-        for label in self.stats:
+        self.labels = labels
+        self.priors = {value[0]: value[1] / float(y.shape[0]) for value in zip(labels, counts)}
+        self.stats  = {label: dict() for label in labels}
+
+        for label in labels:
             for feature in range(X.shape[1]):
-                self.stats[label][feature] = (X[y == label, feature].mean(), X[y == label, feature].var(ddof = 1))
+                self.stats[label][feature] = (X[y == label, feature].mean(), X[y == label, feature].var())
+
 
     def predict(self, X):
-        predictions = np.empty(X.shape[0], dtype = np.int64)
+        predictions = np.empty(X.shape[0])
 
         for i in range(X.shape[0]):
-            posteriors = {}
-            for label in self.priors:
-                posterior = self.priors[label]
-                for feature in range(X[i].shape[0]):
-                    posterior *= self.gaussian(X[i][feature], self.stats[label][feature])
-                posteriors[label] = posterior
+            pmax = -1
+            lmax = None
 
-            predictions[i] = 0 if len(set(posteriors.values())) == 1 else max(posteriors, key = posteriors.get)
+            for label in self.labels:
+                posterior = self.priors[label]
+
+                for feature in range(X[i].shape[0]):
+                    posterior *= self.gaussian(X[i, feature], self.stats[label][feature])
+
+                if posterior > pmax:
+                    pmax = posterior
+                    lmax = label
+
+            predictions[i] = lmax
 
         return predictions
+
 
     def gaussian(self, x, stats):
         return np.exp(-np.square(x - stats[0]) / (2 * stats[1])) / np.sqrt(2 * np.pi * stats[1])
--- src/cowboysmall/ml/classifiers/nb/nb2.py
+++ src/cowboysmall/ml/classifiers/nb/nb2.py
@@ -6,11 +6,13 @@ class NaiveBayes:
     def fit(self, X, y):
         self.priors = y.value_counts() / y.shape[0]
         self.mean   = X.groupby(X.keys()[-1])[X.keys()[:-1]].mean()
-        self.var    = X.groupby(X.keys()[-1])[X.keys()[:-1]].var(ddof = 1)
+        self.var    = X.groupby(X.keys()[-1])[X.keys()[:-1]].var()
+
 
     def predict(self, X):
         posteriors = np.array([self.priors * np.prod(self.gaussian(X.iloc[i, :]), axis = 1) for i in range(X.shape[0])])
-        return np.array([0 if np.all(row == row[0]) else np.argmax(row) for row in posteriors])
+        return np.array([np.argmax(posterior) for posterior in posteriors])
+
 
     def gaussian(self, X):
         return np.exp(-np.square(X - self.mean) / (2 * self.var)) / np.sqrt(2 * np.pi * self.var)
